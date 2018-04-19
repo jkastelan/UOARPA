a19deab4fd4e6e300bd42fa5520ca71d4290558d
Jonathan Kastelan <jlk635@nyu.edu>
2018-04-18 20:15:41 -0400

    adding sample

--- sample/move_file.py
+++ sample/move_file.py
@@ -1,10 +1,25 @@
+from paramiko import SSHClient
+from scp import SCPClient
+
 def move_file():
     Popen("gphoto2 -f -p")
     nfile = len(os.listdir("."))
+
+    
     if nfile>0:  ## are there any files to move
         firstfile = sorted(os.listdir("."))[0] ## label first file
         if nfile==1:
             log("One file named {0} in {1}. Attempt to copy to {2}".format(firstfile, $ORGN, $DSTN)
+            # Ensure you have run key-based authentication setup    
+            ssh = SSHClient()
+            ssh.load_systen_host_keys()
+            # Connect to the server
+            ssh.connect('<server ip>')
+            scp = SCPClient(ssh.get_transport())
+            #Transfer the file from src path to dest path 
+            scp.put('<$ORGN>','<$DSTN>')     
+            #Close the connection
+            scp.close() 
             shutil.copy2($ORGN/firstfile, $DSTN/firstfile)
             log("{0} file copied from {1} to {2}".format(filename, $ORGN, $DSTN))
             log("Attempt to remove {0} from {1}".format(filename, $ORGN))
@@ -12,7 +27,15 @@ def move_file():
             log("file removed from {0}".format($ORGN))
         elif 1 < nfile < $NMAX:
             log("Multiple files in {0} ({1} total). Attempt to copy {2} to {3}.".format($ORGN, nfile, firstfile, $DSTN)
-            shutil.copy2($ORGN/firstfile, $DSTN/firstfile)
+            ssh = SSHClient()
+            ssh.load_systen_host_keys()
+            # Connect to the server
+            ssh.connect('<server ip>')
+            scp = SCPClient(ssh.get_transport())
+            #Transfer the file from src path to dest path 
+            scp.put('<$ORGN>','<$DSTN>')     
+            #Close the connection
+            scp.close()
             log("{0} file copied from {1} to {2}".format(filename, $ORGN, $DSTN))
             log("Attempt to remove {0} from {1}".format(filename, $ORGN)
             os.remove($ORGN/firstfile)
