1b513b35e7986e3e710550d5b40fc74758574976
Jonathan Kastelan <jlk635@nyu.edu>
2018-04-18 22:59:27 -0400

    adding sample

--- sample/move_file.py
+++ sample/move_file.py
@@ -19,25 +19,18 @@ def move_file():
             logging.debug("One file named {0} in {1}. Attempt to copy to {2}".format(firstfile, os.environ["orgn"], os.environ["dstn"])
             #Transfer the file from src path to dest path 
             scp.put('os.environ["orgn"]','os.environ["dstn"]')     
-            #Close the connection
-            scp.close() 
             shutil.copy2('os.environ["orgn"]/firstfile', 'os.environ["dstn"]/firstfile')
             logging.debug("{0} file copied from {1} to {2}".format(filename, os.environ["orgn"], os.environ["dstn"]))
             logging.debug("Attempt to remove {0} from {1}".format(filename, os.environ["orgn"]))
-            os.remove($ORGN/firstfile)
-            log("file removed from {0}".format($ORGN))
+            os.remove('os.environ["orgn"]/firstfile')
+            loggin.debug("file removed from {0}".format(os.environ["orgn"]))
         elif 1 < nfile < os.environ('NMAX'):
             logging.debug("Multiple files in {0} ({1} total). Attempt to copy {2} to {3}.".format(os.environ["orgn"], nfile, firstfile, os.environ["dstn"])
-            ssh = SSHClient()
-            ssh.load_systen_host_keys()
-            # Connect to the server
-            ssh.connect('<server ip>')
-            scp = SCPClient(ssh.get_transport())
             #Transfer the file from src path to dest path 
             scp.put('os.environ["orgn"]','os.environ["dstn"]')
             logging.debug("{0} file copied from {1} to {2}".format(filename, os.environ["orgn"], os.environ["dstn"]))
             logging.debug("Attempt to remove {0} from {1}".format(filename, os.environ["orgn"])
-            os.remove(os.environ["orgn"]/firstfile)
+            os.remove('os.environ["orgn"]/firstfile')
             logging.debug("file removed from {0}. There are {1} files remaining.".format(os.environ["orgn"],nfile-1)
             #MOVE NEXT FILE? (call this function again?)
         else 
