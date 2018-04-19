79a5a86fd291ff30b8e679a900456a06c7d5891d
Jonathan Kastelan <jlk635@nyu.edu>
2018-04-19 10:12:53 -0400

    adding sample

--- sample/move_file.py
+++ sample/move_file.py
@@ -3,16 +3,16 @@ from scp import SCPClient
 import os
 import logging
 
+# Ensure you have run key-based authentication setup    
+ssh = SSHClient()
+ssh.load_systen_host_keys()
+with SCPClient(ssh.get_transport()) as scp:
+
 
 def move_file():
     Popen("gphoto2 -f -p")
     nfile = len(os.listdir("."))
-    
-    # Ensure you have run key-based authentication setup    
-    ssh = SSHClient()
-    ssh.load_systen_host_keys()
-    with SCPClient(ssh.get_transport()) as scp:
-    
+
     if nfile>0:  ## are there any files to move
         firstfile = sorted(os.listdir("."))[0] ## label first file
         if nfile==1:
@@ -23,7 +23,7 @@ def move_file():
             logging.debug("{0} file copied from {1} to {2}".format(filename, os.environ["orgn"], os.environ["dstn"]))
             logging.debug("Attempt to remove {0} from {1}".format(filename, os.environ["orgn"]))
             os.remove('os.environ["orgn"]/firstfile')
-            loggin.debug("file removed from {0}".format(os.environ["orgn"]))
+            logging.debug("file removed from {0}".format(os.environ["orgn"]))
         elif 1 < nfile < os.environ('NMAX'):
             logging.debug("Multiple files in {0} ({1} total). Attempt to copy {2} to {3}.".format(os.environ["orgn"], nfile, firstfile, os.environ["dstn"])
             #Transfer the file from src path to dest path 
@@ -34,7 +34,7 @@ def move_file():
             logging.debug("file removed from {0}. There are {1} files remaining.".format(os.environ["orgn"],nfile-1)
             #MOVE NEXT FILE? (call this function again?)
         else 
-            logging.debug("Greater than {} files".format(os.environ('NMAX')))
+            logging.debug("Greater than {0} files".format(os.environ('NMAX')))
             #DELETE ALL FILES IN ORIGIN?
        
         #Close the connection
